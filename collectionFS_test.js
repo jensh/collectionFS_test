b3ec0f986a3f5f6d08e5e0c595dacb8216c7089f
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2014-04-04 20:55:14 +0200

    Reduced failing code to a minimum

--- collectionFS_test.js
+++ collectionFS_test.js
@@ -1,51 +1,21 @@
 FS.debug = true;
 
-/**********************************************
- * File storages
- **********************************************/
-
-var eventPhotos = new FS.Store.FileSystem('eventPhotos', {
+var eventPhotosStore = new FS.Store.FileSystem('eventPhotos', {
   path: '~/uploads/full'
 });
 
-var thumbnailFormat = {width: 100, height: 100};
-var eventPhotosThumbnails = new FS.Store.FileSystem('eventPhotosThumbnails', {
-  path: '~/uploads/thumbnail',
-  transformWrite: function (file, readStream, writeStream) {
-    this.gm(readStream, file.name)
-      .resize(thumbnailFormat.width, thumbnailFormat.height + '^')
-      .gravity('Center')
-      .crop(thumbnailFormat.width, thumbnailFormat.height)
-      .stream()
-      .pipe(writeStream);
-  }
-});
-
-/**********************************************
- * File collections
- **********************************************/
-
-var defaultPhotoFilter = {
-  maxSize: 5242880, // 5 MB
-  allow: {
-    contentTypes: ['image/*']
-  }
-};
-
 eventPhotos = new FS.Collection('eventPhotos', {
-  stores: [eventPhotosThumbnails, eventPhotos]//,
-  //filter: defaultPhotoFilter
+  stores: [eventPhotosStore]
 });
 eventPhotos.allow({
-  insert: function (userId) {
-    return !!userId;
+  insert: function () {
+    return true;
   },
-  update: function (userId) {
-    return !!userId;
+  update: function () {
+    return true;
   },
   remove: function () {
-    // TODO: Create a method that checks if the photo is still used by other events
-    return false;
+    return true;
   },
   download: function () {
     return true;
